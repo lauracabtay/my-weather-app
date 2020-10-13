d43641165bc3fd8eab3312d96d8d169082d9669c
lauracabtay <45257292+lauracabtay@users.noreply.github.com>
2020-10-13 21:34:14 +0100

    Update URL for weather icon

--- src/script.js
+++ src/script.js
@@ -47,7 +47,7 @@ function showWeather(response) {
   date.innerHTML = formatDate(response.data.dt * 1000);
 
   let icon = document.querySelector("#weather-icon");
-  icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  icon.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   icon.setAttribute("alt", response.data.weather[0].description);
 }
 
